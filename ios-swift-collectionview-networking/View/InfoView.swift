869f059c0b7aa6d2a1b1d0cc72d1fb0386768d21
otaviob <otaviob22@gmail.com>
2025-05-18 17:02:41 +1000

    InfoView Config

--- ios-swift-collectionview-networking/View/InfoView.swift
+++ ios-swift-collectionview-networking/View/InfoView.swift
@@ -14,6 +14,7 @@ class InfoView: UIView {
     let imageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFill
+        iv.backgroundColor = .lightGray
         return iv
     }()
     
@@ -30,36 +31,43 @@ class InfoView: UIView {
         let label = UILabel()
         label.textColor = .white
         label.font = UIFont.systemFont(ofSize: 16)
+        label.text = "TEST001"
         return label
     }()
     
     let typeLabel: UILabel = {
         let label = UILabel()
+        label.text = "TEST001"
         return label
     }()
     
     let defenseLabel: UILabel = {
         let label = UILabel()
+        label.text = "TEST001"
         return label
     }()
     
     let heightLabel: UILabel = {
         let label = UILabel()
+        label.text = "TEST001"
         return label
     }()
     
     let pokedexIdLabel: UILabel = {
         let label = UILabel()
+        label.text = "TEST001"
         return label
     }()
     
     let attackLabel: UILabel = {
         let label = UILabel()
+        label.text = "TEST001"
         return label
     }()
     
     let weightLabel: UILabel = {
         let label = UILabel()
+        label.text = "TEST001"
         return label
     }()
     
@@ -105,6 +113,33 @@ class InfoView: UIView {
         addSubview(imageView)
         imageView.anchor(top: nameContainerView.bottomAnchor, left: nil, bottom: nil, right: nil, paddingTop: 24, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 100, height: 60)
         imageView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        
+        addSubview(typeLabel)
+        typeLabel.anchor(top: imageView.bottomAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 16, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        addSubview(defenseLabel)
+        defenseLabel.anchor(top: imageView.bottomAnchor, left: nil, bottom: nil, right: rightAnchor, paddingTop: 16, paddingLeft: 0, paddingBottom: 0, paddingRight: 8, width: 0, height: 0)
+        
+        let separatorView = UIView()
+        separatorView.backgroundColor = .groupTableViewBackground
+        addSubview(separatorView)
+        separatorView.anchor(top: typeLabel.bottomAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 8, paddingLeft: 4, paddingBottom: 0, paddingRight: 4, width: 0, height: 1)
+        
+        addSubview(heightLabel)
+        heightLabel.anchor(top: separatorView.bottomAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 16, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        addSubview(weightLabel)
+        weightLabel.anchor(top: heightLabel.bottomAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 16, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        addSubview(pokedexIdLabel)
+        pokedexIdLabel.anchor(top: separatorView.bottomAnchor, left: nil, bottom: nil, right: rightAnchor, paddingTop: 16, paddingLeft: 0, paddingBottom: 0, paddingRight: 8, width: 0, height: 0)
+        
+        addSubview(attackLabel)
+        attackLabel.anchor(top: pokedexIdLabel.bottomAnchor, left: nil, bottom: nil, right: rightAnchor, paddingTop: 16, paddingLeft: 0, paddingBottom: 0, paddingRight: 8, width: 0, height: 0)
+        
+        addSubview(infoButton)
+        infoButton.anchor(top: nil, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 12, paddingBottom: 12, paddingRight: 12, width: 0, height: 50)
+        
     }
     
     
